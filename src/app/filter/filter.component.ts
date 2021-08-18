40da5d15264790dc3c0f4acea352e37e1d85ee23
FonTrizz <fontrizz@gmail.com>
2021-08-18 22:06:44 +0700

    In progress of making filters

--- src/app/filter/filter.component.ts
+++ src/app/filter/filter.component.ts
@@ -11,7 +11,7 @@ import {MatTableDataSource} from "@angular/material/table";
 export class FilterComponent implements OnInit {
 
   nameFilter = new FormControl('');
-  ageFilter = new FormControl(0);
+  ageFilter = new FormControl('');
   genderFilter = new FormControl('');
   departmentFilter = new FormControl('');
   cityFilter = new FormControl('');
@@ -71,9 +71,10 @@ export class FilterComponent implements OnInit {
     return function (data, filter): boolean {
       let searchTerms = JSON.parse(filter);
       return data.name.toLowerCase().indexOf(searchTerms.name) !== -1
-        && data.id.toString().toLowerCase().indexOf(searchTerms.id) !== -1
-        && data.colour.toLowerCase().indexOf(searchTerms.colour) !== -1
-        && data.pet.toLowerCase().indexOf(searchTerms.pet) !== -1;
+        && data.age.toString().toLowerCase().indexOf(searchTerms.age) !== -1
+        && data.gender.toLowerCase().indexOf(searchTerms.gender) !== -1
+        && data.department.toLowerCase().indexOf(searchTerms.department) !== -1
+        && data.city.toLowerCase().indexOf(searchTerms.city) !== -1;
     };
   }
 
